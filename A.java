52059f1b6018628aa605eed4f06c3a5b8a52555e
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-16 11:57:03 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,10 @@
+/* Implicit TypeCasting... */
+class A 
+{
+    public static void main(String[] args) {
+        
+        int a=10;  // 4 Bytes
+        double b=a;  // 8 Bytes
+        System.out.print(b);
+    }
+}
